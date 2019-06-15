55f786be36754070cd7b3cf2b5be8f63c0211d8d
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-15 21:06:05 +0800

    update dynamic data

--- Landmarks/LandmarkDetail.swift
+++ Landmarks/LandmarkDetail.swift
@@ -8,25 +8,32 @@
 
 import SwiftUI
 
-struct ContentView : View {
+struct LandmarkDetail : View {
+    var landmark: Landmark
+    
     var body: some View {
-        VStack {
-            
-            MapView()
+        VStack() {
+            MapView(coordinate: landmark.locationCoordinate)
+                // allow the map content to extend to the top edge of the screen
+                .edgesIgnoringSafeArea(.top)
                 .frame(height: 300)
             
-            CircleImage()
+            CircleImage(image: landmark.image(forSize: 250))
                 .offset(y: -130)
                 .padding(.bottom, -130)
             
             VStack(alignment: .leading) {
-                Text("Turtle Rock")
+                Text(landmark.name)
                     .font(.title)
-                HStack {
-                    Text("Joshua Tree National Park")
+                    .color(.black)
+                
+                HStack(alignment: .top) {
+                    Text(landmark.park)
                         .font(.subheadline)
+                    
                     Spacer()
-                    Text("California")
+                    
+                    Text(landmark.state)
                         .font(.subheadline)
                 }
             }
@@ -34,13 +41,14 @@ struct ContentView : View {
             
             Spacer()
         }
+        .navigationBarTitle(Text(landmark.name), displayMode: .inline)
     }
 }
 
 #if DEBUG
 struct ContentView_Previews : PreviewProvider {
     static var previews: some View {
-        ContentView()
+        LandmarkDetail(landmark: landmarkData[0])
     }
 }
 #endif
--- Landmarks/MapView.swift
+++ Landmarks/MapView.swift
@@ -11,13 +11,13 @@ import MapKit
 
 struct MapView : UIViewRepresentable {
     
+    var coordinate: CLLocationCoordinate2D
+    
     func makeUIView(context: Context) -> MKMapView {
         MKMapView(frame: .zero)
     }
     
     func updateUIView(_ view: MKMapView, context: Context) {
-        let coordinate = CLLocationCoordinate2D(
-            latitude: 34.011286, longitude: -116.166868)
         let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
         let region = MKCoordinateRegion(center: coordinate, span: span)
         view.setRegion(region, animated: true)
@@ -27,7 +27,7 @@ struct MapView : UIViewRepresentable {
 #if DEBUG
 struct MapView_Previews : PreviewProvider {
     static var previews: some View {
-        MapView()
+        MapView(coordinate: landmarkData[0].locationCoordinate)
     }
 }
 #endif
