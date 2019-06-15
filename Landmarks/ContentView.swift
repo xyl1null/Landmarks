5c018d02cb4458f8a78ab3b4f6693a63873c758b
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-15 18:30:39 +0800

    Initial Commit

---
+++ Landmarks/ContentView.swift
@@ -0,0 +1,23 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 15/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView : View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+#if DEBUG
+struct ContentView_Previews : PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
+#endif
