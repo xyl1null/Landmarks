41c97291a03f58b063eb1590312bc4bf063814c9
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-15 21:06:43 +0800

    add landmark list view

---
+++ Landmarks/LandmarkList.swift
@@ -0,0 +1,34 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 15/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList : View {
+    var body: some View {
+        NavigationView {
+            List(landmarkData) { landmark in
+                NavigationButton(destination: LandmarkDetail(landmark: landmark)) {
+                    LandmarkRow(landmark: landmark)
+                }
+            }
+            .navigationBarTitle(Text("Landmarks"))
+        }
+    }
+}
+
+struct LandmarkList_Previews : PreviewProvider {
+    static var previews: some View {
+
+        ForEach(["iPhone SE", "iPhone XS Max"].identified(by: \.self)) { deviceName in
+            LandmarkList()
+                .previewDevice(PreviewDevice(rawValue: deviceName))
+                .previewDisplayName(deviceName)
+            
+        }
+    }
+}
