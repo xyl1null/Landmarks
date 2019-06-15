7650b0f4c12f3f35b184e640b6ab98361f2203ab
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-15 23:54:32 +0800

    add a bindable object for storage

---
+++ Landmarks/UserData.swift
@@ -0,0 +1,27 @@
+//
+//  UserData.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 15/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+import Combine
+
+final class UserData: BindableObject {
+    let didChange = PassthroughSubject<UserData, Never>()
+    
+    var showFavouritesOnly = false {
+        didSet {
+            didChange.send(self)
+        }
+    }
+    
+    var landmarks = landmarkData {
+        didSet {
+            didChange.send(self)
+        }
+    }
+}
+
