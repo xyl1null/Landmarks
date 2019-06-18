43a6f8d8afe54db7e140dccc0eb1bcdd706d1f60
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-18 16:46:05 +0800

    add hikeView

---
+++ Landmarks/Hike/HikeView.swift
@@ -0,0 +1,69 @@
+//
+//  HikeView.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 18/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct HikeView: View {
+    var hike: Hike
+    @State private var showDetail = false
+    
+    var transition: AnyTransition {
+        let insertion = AnyTransition.move(edge: .trailing)
+            .combined(with: .opacity)
+        let removal = AnyTransition.scale()
+            .combined(with: .opacity)
+        return .asymmetric(insertion: insertion, removal: removal)
+    }
+    
+    var body: some View {
+        VStack {
+            HStack {
+                HikeGraph(hike: hike, path: \.elevation)
+                    .frame(width: 50, height: 30)
+                    .animation(nil)
+                
+                VStack(alignment: .leading) {
+                    Text(verbatim: hike.name)
+//                        .font(.headline)
+                    Text(verbatim: hike.distanceText)
+                }
+                
+                Spacer()
+                
+                Button(action: {
+                    withAnimation(.basic(duration: 2.5)) {
+                        self.showDetail.toggle()
+                    }
+                }) {
+                    Image(systemName: "chevron.right.circle")
+                        .imageScale(.large)
+                        .rotationEffect(.degrees(showDetail ? 90 : 0))
+                        .scaleEffect(showDetail ? 1.5 : 1)
+                        .padding()
+                }
+            }
+            
+            if showDetail {
+                HikeDetail(hike: hike)
+                    .transition(transition)
+            }
+        }
+    }
+}
+
+#if DEBUG
+struct HikeView_Previews: PreviewProvider {
+    static var previews: some View {
+        VStack {
+            HikeView(hike: hikeData[0])
+                .padding()
+            Spacer()
+        }
+    }
+}
+#endif
