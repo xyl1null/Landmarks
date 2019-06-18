91aa021dd3739a653a13c6fe5a35f56f130bd9ed
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-18 16:49:40 +0800

    add profileView

---
+++ Landmarks/Profile/ProfileEditor.swift
@@ -0,0 +1,57 @@
+//
+//  ProfileEditor.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 18/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct ProfileEditor : View {
+    @Binding var profile: Profile
+    
+    var body: some View {
+        List {
+            HStack {
+                Text("Username").bold()
+                Divider()
+                TextField($profile.username)
+            }
+            
+            Toggle(isOn: $profile.prefersNotifications) {
+                Text("Enable Notifications")
+            }
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Seasonal Photo").bold()
+                
+                SegmentedControl(selection: $profile.seasonalPhoto) {
+                    ForEach(Profile.Season.allCases.identified(by: \.self)) { season in
+                        Text(season.rawValue).tag(season)
+                    }
+                }
+            }
+            .padding(.top)
+            
+            VStack(alignment: .leading, spacing: 20) {
+                Text("Goal Date").bold()
+                DatePicker(
+                    $profile.goalDate,
+                    minimumDate: Calendar.current.date(byAdding: .year, value: -1, to: profile.goalDate),
+                    maximumDate: Calendar.current.date(byAdding: .year, value: 1, to: profile.goalDate),
+                    displayedComponents: .date
+                )
+            }
+            .padding(.top)
+        }
+    }
+}
+
+#if DEBUG
+struct ProfileEditor_Previews : PreviewProvider {
+    static var previews: some View {
+        ProfileEditor(profile: .constant(.default))
+    }
+}
+#endif
