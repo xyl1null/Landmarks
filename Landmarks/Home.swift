d204a168e3550b48ed39ef95647be0889afe79e4
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-18 16:52:22 +0800

    add home View

---
+++ Landmarks/Home.swift
@@ -0,0 +1,77 @@
+//
+//  Home.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 17/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeaturedLandmarks: View {
+    var landmarks: [Landmark]
+    var body: some View {
+        landmarks[0].image(forSize: 250).resizable()
+    }
+}
+
+struct CategoryHome : View {
+    
+    var categories: [String: [Landmark]] {
+        .init(
+            grouping: landmarkData,
+            by: { $0.category.rawValue }
+        )
+    }
+    
+    var featured: [Landmark] {
+        landmarkData.filter { $0.isFeatured }
+    }
+
+    var body: some View {
+        NavigationView {
+            List {
+//                ForEach(categories.keys.sorted().identified(by: \.self)) { key in
+//                    FeaturedLandmarks(landmarks: self.featured)
+//                        .scaledToFill()
+//                        .frame(height: 200)
+//                        .clipped()
+//
+//                }
+//                .listRowInsets(EdgeInsets())
+                FeaturedLandmarks(landmarks: featured)
+                    .scaledToFill()
+                    .frame(height: 200)
+                    .clipped()
+                    .listRowInsets(EdgeInsets())
+                
+                ForEach(categories.keys.sorted().identified(by: \.self)) { key in
+                    CategoryRow(categoryName: key, items: self.categories[key]!)
+                }
+                .listRowInsets(EdgeInsets())
+                
+                NavigationButton(destination: LandmarkList()) {
+                    Text("See All")
+                }
+            }
+            .navigationBarTitle(Text("Featured"))
+            .navigationBarItems(trailing:
+                PresentationButton(
+                    Image(systemName: "person.crop.circle")
+                        .imageScale(.large)
+                        .accessibility(label: Text("User Profile"))
+                        .padding(),
+                    destination: ProfileHost()
+                )
+            )
+        }
+    }
+}
+
+#if DEBUG
+struct CategoryHome_Previews : PreviewProvider {
+    static var previews: some View {
+        CategoryHome()
+    }
+}
+#endif
