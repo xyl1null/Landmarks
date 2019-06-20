beddbf6223ac0e57640f0ed949145013bc79894f
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-20 08:48:29 +0800

    add feature card

---
+++ Landmarks/FeatureCard.swift
@@ -0,0 +1,54 @@
+//
+//  FeatureCard.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 19/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+import SwiftUI
+
+struct FeatureCard: View {
+    var landmark: Landmark
+    
+    var body: some View {
+        landmark.featureImage?
+            .resizable()
+            .aspectRatio(3 / 2, contentMode: .fit)
+            .overlay(TextOverlay(landmark: landmark))
+    }
+}
+
+struct TextOverlay: View {
+    var landmark: Landmark
+    
+    var gradient: LinearGradient {
+        LinearGradient(
+            gradient: Gradient(
+                colors: [Color.black.opacity(0.6), Color.black.opacity(0)]),
+            startPoint: .bottom,
+            endPoint: .center)
+    }
+    
+    var body: some View {
+        ZStack(alignment: .bottomLeading) {
+            Rectangle().fill(gradient)
+            VStack(alignment: .leading) {
+                Text(landmark.name)
+                    .font(.title)
+                    .bold()
+                Text(landmark.park)
+                }
+                .padding()
+            }
+            .foregroundColor(.white)
+    }
+}
+
+#if DEBUG
+struct FeatureCard_Previews: PreviewProvider {
+    static var previews: some View {
+        FeatureCard(landmark: features[0])
+    }
+}
+#endif
