4b2fdc2342ef17e86e736d95650e642747d532bf
leighshanie <yao.h.wang@uni.sydney.edu.au>
2019-06-20 08:50:00 +0800

    add feature page view

---
+++ Landmarks/PageControl.swift
@@ -0,0 +1,47 @@
+//
+//  PageControl.swift
+//  Landmarks
+//
+//  Created by Shane Leigh on 19/06/2019.
+//  Copyright © 2019 SoundWorm LLC. All rights reserved.
+//
+
+
+import SwiftUI
+import UIKit
+
+struct PageControl: UIViewRepresentable {
+    var numberOfPages: Int
+    @Binding var currentPage: Int
+    
+    func makeCoordinator() -> Coordinator {
+        Coordinator(self)
+    }
+    
+    func makeUIView(context: Context) -> UIPageControl {
+        let control = UIPageControl()
+        control.numberOfPages = numberOfPages
+        control.addTarget(
+            context.coordinator,
+            action: #selector(Coordinator.updateCurrentPage(sender:)),
+            for: .valueChanged)
+        
+        return control
+    }
+    
+    func updateUIView(_ uiView: UIPageControl, context: Context) {
+        uiView.currentPage = currentPage
+    }
+    
+    class Coordinator: NSObject {
+        var control: PageControl
+        
+        init(_ control: PageControl) {
+            self.control = control
+        }
+        
+        @objc func updateCurrentPage(sender: UIPageControl) {
+            control.currentPage = sender.currentPage
+        }
+    }
+}
